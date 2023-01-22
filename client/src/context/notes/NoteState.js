8487aa27ab19b6c9ce78e4b6255fecccf15a25dd
abhinav_700 <abhinavagrawal700@gmail.com>
2023-01-22 17:12:14 +0530

    linked frontend apis to deployed backend

--- client/src/context/notes/NoteState.js
+++ client/src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 const NoteState = (props) => {
 
-  const host = "http://localhost:5000";
+  const host = "https://notekeeper-backend.onrender.com";
   const initialNotes = [];
   const [notes, setNotes] = useState(initialNotes);
 
